728452a3370f25ebbc0d2e6f40d5361592b2ef3e
pacer <pacer97@163.com>
2025-05-28 14:50:03 +0000

    refactor: improve code formatting and consistency in DividerTest class

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,47 +3,68 @@
 
 #define VI std::vector<int>
 
-class DividerTest : public ::testing::Test {
-  protected:
-    VI numerators = {5, 9, 17, 9999};
+class DividerTest : public ::testing::Test
+{
+protected:
+    VI numerators   = {5, 9, 17, 9999};
     VI denominators = {2, 3, 19, 10000};
-    VI divisions = {2, 3, 0, 0};
-    VI remainders = {1, 0, 17, 9999};
+    VI divisions    = {2, 3, 0, 0};
+    VI remainders   = {1, 0, 17, 9999};
 
-    virtual void SetUp(){};
+    virtual void SetUp() {};
 
-    virtual void TearDown(){};
+    virtual void TearDown() {};
 
-    virtual void verify(int index) {
-        Fraction f = Fraction{numerators.at(index), denominators.at(index)};
+    virtual void verify(int index)
+    {
+        Fraction       f        = Fraction{numerators.at(index), denominators.at(index)};
         DivisionResult expected = DivisionResult{divisions.at(index), remainders.at(index)};
-        DivisionResult result = Division(f).divide();
+        DivisionResult result   = Division(f).divide();
         EXPECT_EQ(result.division, expected.division);
         EXPECT_EQ(result.remainder, expected.remainder);
     }
 };
 
-TEST_F(DividerTest, 5_DivideBy_2) { verify(0); }
+TEST_F(DividerTest, 5_DivideBy_2)
+{
+    verify(0);
+}
 
-TEST_F(DividerTest, 9_DivideBy_3) { verify(1); }
+TEST_F(DividerTest, 9_DivideBy_3)
+{
+    verify(1);
+}
 
-TEST_F(DividerTest, 17_DivideBy_19) { verify(2); }
+TEST_F(DividerTest, 17_DivideBy_19)
+{
+    verify(2);
+}
 
-TEST_F(DividerTest, 9999_DivideBy_10000) { verify(3); }
+TEST_F(DividerTest, 9999_DivideBy_10000)
+{
+    verify(3);
+}
 
-TEST_F(DividerTest, DivisionByZero) {
+TEST_F(DividerTest, DivisionByZero)
+{
     Division d = Division(Fraction{1, 0});
-    try {
+    try
+    {
         d.divide();
         FAIL() << "Expected divide() method to throw DivisionByZeroException";
-    } catch (DivisionByZero excp) {
+    }
+    catch (DivisionByZero excp)
+    {
         EXPECT_EQ(excp.what(), "Division by zero is illegal");
-    } catch (...) {
+    }
+    catch (...)
+    {
         FAIL() << "Expected DivisionByZeroException!";
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
